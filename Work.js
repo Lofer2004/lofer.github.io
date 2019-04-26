45e27e7b856ed416336af3bfb01b36cc3545df2c
Lofer2004 <50028726+Lofer2004@users.noreply.github.com>
2019-04-26 21:23:38 +0300

    Add files via upload

---
+++ Work.js
@@ -0,0 +1,138 @@
+function WorkClick(){
+    var work = document.getElementById("work");
+    work.style.display = "none";
+    var nav1 = document.getElementById("nav1");
+    nav1.style.display = "block";
+    
+
+}
+
+function Podorosh(){
+       var nav2 = document.getElementById("nav2");
+         var nav1 = document.getElementById("nav1");
+         nav1.style.display ="none";
+       nav2.style.display = "block";
+ }
+
+function Pomoch(){
+       var nav3 = document.getElementById("nav3");
+         var nav1 = document.getElementById("nav1");
+         nav1.style.display ="none";
+       nav3.style.display = "block";
+ }
+
+function Xudoshnik(){
+           var nav4 = document.getElementById("nav4");
+         var nav1 = document.getElementById("nav1");
+         nav1.style.display ="none";
+       nav4.style.display = "block"; 
+}
+
+function Konstruktor(){
+           var nav5 = document.getElementById("nav5");
+         var nav1 = document.getElementById("nav1");
+         nav1.style.display ="none";
+       nav5.style.display = "block"; 
+}
+
+function Arxeolox(){
+    var prof1 = document.getElementById("prof1");
+    prof1.style.display = "block";
+    var nav2 = document.getElementById("nav2");
+    nav2.style.display = "none";
+}
+
+function Geolox(){
+      var prof2 = document.getElementById("prof2");
+    prof2.style.display = "block";
+    var nav2 = document.getElementById("nav2");
+    nav2.style.display = "none";
+}
+
+function Turist(){
+      var prof3 = document.getElementById("prof3");
+    prof3.style.display = "block";
+    var nav2 = document.getElementById("nav2");
+    nav2.style.display = "none";
+}
+
+function English(){
+      var prof4 = document.getElementById("prof4");
+    prof4.style.display = "block";
+    var nav2 = document.getElementById("nav2");
+    nav2.style.display = "none";
+}
+
+function Newpeppar(){
+      var prof5 = document.getElementById("prof5");
+    prof5.style.display = "block";
+    var nav2 = document.getElementById("nav2");
+    nav2.style.display = "none";
+}
+
+function Vrach(){
+      var prof6 = document.getElementById("prof6");
+    prof6.style.display = "block";
+    var nav3 = document.getElementById("nav3");
+    nav3.style.display = "none";
+}
+
+function Volonter(){
+      var prof7 = document.getElementById("prof7");
+    prof7.style.display = "block";
+    var nav3 = document.getElementById("nav3");
+    nav3.style.display = "none";
+}
+
+function Veterinar(){
+      var prof8 = document.getElementById("prof8");
+    prof8.style.display = "block";
+    var nav3 = document.getElementById("nav3");
+    nav3.style.display = "none";
+}
+
+function Dizain(){
+      var prof9 = document.getElementById("prof9");
+    prof9.style.display = "block";
+    var nav4 = document.getElementById("nav4");
+    nav4.style.display = "none";
+}
+
+function Aktor(){
+      var prof10 = document.getElementById("prof10");
+    prof10.style.display = "block";
+    var nav4 = document.getElementById("nav4");
+    nav4.style.display = "none";
+}
+
+
+function Photo(){
+      var prof11 = document.getElementById("prof11");
+    prof11.style.display = "block";
+    var nav4 = document.getElementById("nav4");
+    nav4.style.display = "none";
+}
+
+
+function Programmer(){
+      var prof12 = document.getElementById("prof12");
+    prof12.style.display = "block";
+    var nav5 = document.getElementById("nav5");
+    nav5.style.display = "none";
+}
+
+function Inshener(){
+      var prof13 = document.getElementById("prof13");
+    prof13.style.display = "block";
+    var nav5 = document.getElementById("nav5");
+    nav5.style.display = "none";
+}
+
+function Working(){
+      var prof14 = document.getElementById("prof14");
+    prof14.style.display = "block";
+    var nav5 = document.getElementById("nav5");
+    nav5.style.display = "none";
+    
+}
+
